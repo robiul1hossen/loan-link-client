e0514bdb17aa676d71a848db1126598b52492da6
Mohammad Robiul Hossen <robim6225@gmail.com>
2025-12-09 00:24:12 +0600

    add axios interceptor instance

--- src/hooks/useAxiosSecure.jsx
+++ src/hooks/useAxiosSecure.jsx
@@ -1,10 +1,39 @@
 import axios from "axios";
-import React from "react";
+import React, { use, useEffect } from "react";
+import { AuthContext } from "../context/AuthContext";
+import { useNavigate } from "react-router";
 
+const axiosSecure = axios.create({
+  baseURL: "http://localhost:3000",
+});
 const useAxiosSecure = () => {
-  const axiosSecure = axios.create({
-    baseURL: "http://localhost:3000",
-  });
+  const { user, logOutUser } = use(AuthContext);
+  const navigate = useNavigate();
+  useEffect(() => {
+    const reqInterceptor = axiosSecure.interceptors.request.use((config) => {
+      config.headers.Authorization = `Bearer ${user?.accessToken}`;
+      return config;
+    });
+
+    const resInterceptor = axiosSecure.interceptors.response.use(
+      (response) => {
+        return response;
+      },
+      (error) => {
+        const statusCode = error.status;
+        if (statusCode === 401 || statusCode === 403) {
+          logOutUser().then(() => {
+            navigate("/auth/login");
+          });
+        }
+        return new Promise.reject(error);
+      }
+    );
+    return () => {
+      axiosSecure.interceptors.request.eject(reqInterceptor);
+      axiosSecure.interceptors.response.eject(resInterceptor);
+    };
+  }, [user, logOutUser, navigate]);
   return axiosSecure;
 };
 
