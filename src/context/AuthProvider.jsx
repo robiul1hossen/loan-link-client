3ca00544694f426e772a5ddd2a7e9296ccaa23a6
Mohammad Robiul Hossen <robim6225@gmail.com>
2025-12-10 21:42:27 +0600

    add google authentication

--- src/context/AuthProvider.jsx
+++ src/context/AuthProvider.jsx
@@ -2,12 +2,16 @@ import React, { useEffect, useState } from "react";
 import { AuthContext } from "./AuthContext";
 import {
   createUserWithEmailAndPassword,
+  GoogleAuthProvider,
   onAuthStateChanged,
   signInWithEmailAndPassword,
+  signInWithPopup,
   signOut,
   updateProfile,
 } from "firebase/auth";
 import { auth } from "../firebase/firebase.config";
+
+const googleProvider = new GoogleAuthProvider();
 
 const AuthProvider = ({ children }) => {
   const [user, setUser] = useState(null);
@@ -21,6 +25,11 @@ const AuthProvider = ({ children }) => {
   const loginUser = (email, password) => {
     setLoading(true);
     return signInWithEmailAndPassword(auth, email, password);
+  };
+
+  const loginWithGoogle = () => {
+    setLoading(true);
+    return signInWithPopup(auth, googleProvider);
   };
 
   const logOutUser = () => {
@@ -43,6 +52,7 @@ const AuthProvider = ({ children }) => {
   const userInfo = {
     registerUser,
     loginUser,
+    loginWithGoogle,
     updateUser,
     logOutUser,
     user,
--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -6,7 +6,7 @@ import { Link, useLocation, useNavigate } from "react-router";
 import useAxiosSecure from "../hooks/useAxiosSecure";
 
 const Register = () => {
-  const { registerUser, updateUser } = use(AuthContext);
+  const { registerUser, updateUser, loginWithGoogle } = use(AuthContext);
   const location = useLocation();
   const navigate = useNavigate();
   const axiosSecure = useAxiosSecure();
@@ -60,17 +60,44 @@ const Register = () => {
         console.log(error);
       });
   };
+  const handleGoogleLogin = () => {
+    loginWithGoogle()
+      .then((res) => {
+        console.log(res.user);
+        const userInfoToDB = {
+          displayName: res.user.displayName,
+          photoURL: res.user.photoURL,
+          email: res.user.email,
+          role: "Borrower",
+        };
+        axiosSecure.post("/users", userInfoToDB).then((res) => {
+          console.log(res.data);
+        });
+        // update user
+        const updateUserInfo = {
+          displayName: res.user.displayName,
+          photoURL: res.user.photoURL,
+        };
+        updateUser(updateUserInfo)
+          .then(() => {
+            console.log("user updated");
+            navigate(`${location?.state ? location?.state : "/"}`);
+          })
+          .catch((error) => console.log("user update error", error));
+      })
+      .catch((error) => console.log(error));
+  };
   return (
     <div>
       <h2 className="text-2xl font-bold text-center mb-4">Sign Up Now!</h2>
-      <div className="card-body rounded-2xl px-20 shadow-2xl">
+      <div className=" px-20 ">
         <form onSubmit={handleSubmit(handleRegister)}>
           <fieldset className="fieldset">
             <label className="label">Name</label>
             <input
               type="text"
               {...register("name", { required: true })}
-              className="input outline-none w-full"
+              className="input outline-none w-full shadow-xl"
               placeholder="Your Name"
             />
             {errors.name && (
@@ -81,7 +108,7 @@ const Register = () => {
             <input
               type="email"
               {...register("email", { required: true })}
-              className="input outline-none w-full"
+              className="input outline-none w-full shadow-xl"
               placeholder="Email"
             />
             {errors.email && (
@@ -91,7 +118,7 @@ const Register = () => {
             <input
               type="password"
               {...register("password", { required: true })}
-              className="input outline-none w-full"
+              className="input outline-none w-full shadow-xl"
               placeholder="Password"
             />
             {errors.password && (
@@ -106,7 +133,7 @@ const Register = () => {
                 <input
                   type="file"
                   {...register("photo", { required: true })}
-                  className="file-input file-input-primary outline-none w-full"
+                  className="file-input file-input-primary outline-none w-full shadow-xl"
                 />
                 {errors.photo && (
                   <span className="text-xs text-red-500">
@@ -117,7 +144,7 @@ const Register = () => {
               <div className="flex-1">
                 <label className="label">Select Your Role</label>
                 <select
-                  className="select select-primary"
+                  className="select select-primary shadow-xl"
                   defaultValue=""
                   {...register("role", { required: true })}>
                   <option value="" disabled>
@@ -133,7 +160,36 @@ const Register = () => {
               </div>
             </div>
 
-            <button className="btn btn-primary mt-4 w-full">Sign up</button>
+            <button className="btn btn-primary mt-4 w-full shadow-xl">
+              Sign up
+            </button>
+            <button
+              onClick={handleGoogleLogin}
+              className="btn bg-white text-black border-[#e5e5e5] shadow-xl">
+              <svg
+                aria-label="Google logo"
+                width="16"
+                height="16"
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 512 512">
+                <g>
+                  <path d="m0 0H512V512H0" fill="#fff"></path>
+                  <path
+                    fill="#34a853"
+                    d="M153 292c30 82 118 95 171 60h62v48A192 192 0 0190 341"></path>
+                  <path
+                    fill="#4285f4"
+                    d="m386 400a140 175 0 0053-179H260v74h102q-7 37-38 57"></path>
+                  <path
+                    fill="#fbbc02"
+                    d="m90 341a208 200 0 010-171l63 49q-12 37 0 73"></path>
+                  <path
+                    fill="#ea4335"
+                    d="m153 219c22-69 116-109 179-50l55-54c-78-75-230-72-297 55"></path>
+                </g>
+              </svg>
+              Login with Google
+            </button>
           </fieldset>
           <p>
             Already have an account? Please{" "}
