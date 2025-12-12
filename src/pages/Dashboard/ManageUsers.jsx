b2e0b5d399f6c9cf8dda4ad933b57890862f682d
Mohammad Robiul Hossen <robim6225@gmail.com>
2025-12-12 18:54:32 +0600

    add search functionality for manage users page

--- src/pages/Dashboard/ManageUsers.jsx
+++ src/pages/Dashboard/ManageUsers.jsx
@@ -1,4 +1,4 @@
-import React, { use } from "react";
+import React, { use, useEffect, useState } from "react";
 import Title from "../../components/Title";
 import { AuthContext } from "../../context/AuthContext";
 import useAxiosSecure from "../../hooks/useAxiosSecure";
@@ -7,6 +7,7 @@ import { Link } from "react-router";
 
 const ManageUsers = () => {
   const { user } = use(AuthContext);
+  const [searchData, setSearchData] = useState([]);
   const axiosSecure = useAxiosSecure();
   const { data: users = [] } = useQuery({
     queryKey: ["users"],
@@ -15,7 +16,17 @@ const ManageUsers = () => {
       return res.data;
     },
   });
-
+  useEffect(() => {
+    setSearchData(users);
+  }, [users]);
+  const handleSearch = async (e) => {
+    const keyword = e.target.value;
+    await axiosSecure.get(`/search/user?keyword=${keyword}`).then((res) => {
+      // console.log(res.data);
+      setSearchData(res.data);
+    });
+  };
+  // console.log("from const", handleSearch);
   return (
     <div>
       <div className="mt-5">
@@ -26,7 +37,15 @@ const ManageUsers = () => {
             "  Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit perferendis, numquam animi mollitia ab ducimus. Perspiciatis error nostrum officiis culpa!"
           }
         />
-
+        <div className="md:text-right my-4">
+          <input
+            type="text"
+            // onChange={(e) => setSearch(e.target.value)}
+            onChange={(e) => handleSearch(e)}
+            placeholder="Find your loan here"
+            className="input outline-none "
+          />
+        </div>
         <div className="overflow-x-auto">
           <table className="table table-zebra">
             {/* head */}
@@ -41,7 +60,7 @@ const ManageUsers = () => {
               </tr>
             </thead>
             <tbody>
-              {users.map((user, i) => (
+              {searchData.map((user, i) => (
                 <tr key={user._id}>
                   <th>{i + 1}</th>
                   <td>{user.displayName}</td>
